1f60e141f413b09876a2f7977a5760d85ecfec64
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-08-31 14:52:09 +0530

    env

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(express.json())
 app.use("/product",ProductController)
 app.use("/review",ReviewController)
 
+const port = process.env.PORT || 8080
 
-
-app.listen(8080,async()=>{
+app.listen(port,async()=>{
     await connect()
     console.log("listening on 8080")
 })
